cd0c20f68500b7dcabb3106a487532aa5d4423d5
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-01-19 15:07:09 -0300

    ajustando `Header` no mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,34 +10,35 @@ export const Header = () => {
             alt="Logo do site"
             width={150}
             height={70}
-            className="relative top-10"
           />
         </div>
-        <div className="w-10/12 mx-auto">
+        <div className="w-10/12 mx-auto relative bottom-6">
           <div className="text-4xl text-center font-bold mb-4">Musculação</div>
           <p className="text-justify">
             Aqui é onde os limites deixam de existir. Cada repetição é uma
             conquista, cada gota de suor é progresso. A musculação é a base da
             força, da forma e da confiança.
           </p>
-          <a href="">
-            <button>Ver na loja</button>
+          <a href="" className="border p-2 bg-white">
+            <button className="text-black uppercase mt-6 text-sm">
+              Ver na loja
+            </button>
           </a>
         </div>
         <div className="relative">
           <Image
             src="/assets/02.png"
             alt="Logo do site"
-            width={450}
+            width={386}
             height={70}
-            className="mt-0.5"
+            className="absolute top-[3px] left-7"
           />
           <Image
             src="/assets/09.png"
             alt="Logo do site"
-            width={550}
+            width={386}
             height={70}
-            className="absolute top-0 left-0 -z-10"
+            className="absolute top-0.5 left-7 -z-10"
           />
         </div>
       </div>
